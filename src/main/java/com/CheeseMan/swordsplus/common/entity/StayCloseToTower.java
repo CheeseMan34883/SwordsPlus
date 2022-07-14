30c6e5f43646f117e27a97a81364d1e858d1ec20
CheeseMan34883 <purpleturtleyy@gmail.com>
2022-07-14 11:07:45 -0500

    Revert "Revert "STay close to Tower Goal, maybe has some erros, i havent tested it yet""
    This reverts commit 32fc5210fb7fb9c5eecdee2b83cf28cb7bbe613d.

---
+++ src/main/java/com/CheeseMan/swordsplus/common/entity/StayCloseToTower.java
@@ -0,0 +1,91 @@
+package com.CheeseMan.swordsplus.common.entity;
+
+import com.CheeseMan.swordsplus.core.init.StructureInit;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.CreatureEntity;
+import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.vector.Vector3f;
+import net.minecraft.world.server.ServerWorld;
+
+import javax.annotation.Nullable;
+import java.util.Comparator;
+
+public class StayCloseToTower extends WaterAvoidingRandomWalkingGoal {
+    //maybe make a config for it
+    public static final int RADIUS = 70;
+
+    protected final WizardEntity wizard;
+
+    public StayCloseToTower(WizardEntity entity, double speedModifier) {
+        super(entity, speedModifier);
+        this.wizard = entity;
+    }
+
+    public StayCloseToTower(WizardEntity entity, double speedModifier, float probability) {
+        super(entity, speedModifier, probability);
+        this.wizard = entity;
+    }
+
+    @Override
+    public boolean canUse() {
+        if (this.mob.isVehicle()) {
+            return false;
+        } else {
+            if (!this.forceTrigger) {
+                if (this.mob.getNoActionTime() >= 100) {
+                    return false;
+                }
+
+                if (this.mob.getRandom().nextInt(this.interval) != 0) {
+                    return false;
+                }
+            }
+            Vector3d vector3d;
+            if (wizard.hasFoundTower()) {
+                vector3d = this.getPosition();
+            }else{
+                vector3d = this.findNearestTower();
+            }
+            if (vector3d == null) {
+                return false;
+            } else {
+                this.wantedX = vector3d.x;
+                this.wantedY = vector3d.y;
+                this.wantedZ = vector3d.z;
+                this.forceTrigger = false;
+                return true;
+            }
+        }
+    }
+
+    protected Vector3d findNearestTower(){
+        if (!wizard.level.isClientSide()) {
+            BlockPos nearestStructure = ((ServerWorld)(wizard.level)).findNearestMapFeature(StructureInit.WIZARD_TOWER.get(), new BlockPos(wizard.position()), 100, true);
+            if (nearestStructure != null){
+                //TODO change the cauldron to ur custom block
+                BlockPos cauldron = BlockPos.betweenClosedStream(new AxisAlignedBB(nearestStructure).inflate(50)).filter(pos -> wizard.level.getBlockState(pos).getBlock() == Blocks.CAULDRON).min(Comparator.comparing(pos -> pos.distSqr(nearestStructure))).orElse(BlockPos.ZERO);
+                wizard.setWizardTarget(cauldron);
+                return convert(cauldron);
+            }
+        }
+        return Vector3d.ZERO;
+    }
+
+    public static Vector3d convert(BlockPos pos){
+        return new Vector3d(pos.getX(), pos.getY(), pos.getZ());
+    }
+
+    @Nullable
+    @Override
+    protected Vector3d getPosition() {
+        Vector3d pos = super.getPosition();
+        while (pos == null || pos.distanceToSqr(convert(wizard.getWizardTarget())) <Math.pow(RADIUS, 2)){
+            pos = super.getPosition();
+        }
+        return pos;
+    }
+}
