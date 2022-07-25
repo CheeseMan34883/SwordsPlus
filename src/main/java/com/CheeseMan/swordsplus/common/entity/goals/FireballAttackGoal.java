8e604e9d315039c353242257bcfe9cf2291838a7
CheeseMan34883 <purpleturtleyy@gmail.com>
2022-07-25 11:39:49 -0500

    Lightning almost works

--- src/main/java/com/CheeseMan/swordsplus/common/entity/goals/FireballAttackGoal.java
+++ src/main/java/com/CheeseMan/swordsplus/common/entity/goals/FireballAttackGoal.java
@@ -14,12 +14,10 @@ import net.minecraft.world.World;
 
 public class FireballAttackGoal extends Goal{
 
-    public static final int LIGHTNING_COOLDOWN = 30;
-
+    
 	private final WizardEntity wizard;
 	private int chargeTime;
-    private boolean lightning = false;
-	
+    
 	public FireballAttackGoal(WizardEntity wizard) {
 		this.wizard = wizard;
 		
@@ -33,7 +31,7 @@ public class FireballAttackGoal extends Goal{
 	@Override
 	public void start() {
 		this.chargeTime = 0;
-        lightning = this.wizard.getRandom().nextDouble() < 0.3;
+        
 	}
 	
 	@Override
@@ -43,10 +41,8 @@ public class FireballAttackGoal extends Goal{
 	
 	@Override
 	public void tick() {
-        if(lightning)
-            lightningTick();
-        else
-            fireballTick();
+            
+		fireballTick();
 	}
 
     private void fireballTick(){
@@ -58,7 +54,7 @@ public class FireballAttackGoal extends Goal{
             if (this.chargeTime == 5 && !this.wizard.isSilent()) {
                 //world.levelEvent((PlayerEntity)null, 1015, this.wizard.blockPosition(), 0);
             }
-            if (this.chargeTime == 10) {
+            if (this.chargeTime == 20) {
                 double d1 = 4.0D;
                 Vector3d vector3d = this.wizard.getViewVector(1.0F);
                 double d2 = entity.getX() - (this.wizard.getX() + vector3d.x * 4.0D);
@@ -67,11 +63,16 @@ public class FireballAttackGoal extends Goal{
                 if (!this.wizard.isSilent()) {
                     world.levelEvent((PlayerEntity)null, 1016, this.wizard.blockPosition(), 0);
                 }
-
+                
+                LightningBoltEntity lightningboltentity = EntityType.LIGHTNING_BOLT.create(world);
+                lightningboltentity.setDamage(wizard.getLightningDamage());
+                lightningboltentity.setPos(this.wizard.getTarget().getX(), this.wizard.getTarget().getY(), this.wizard.getTarget().getZ());
+                world.addFreshEntity(lightningboltentity);
                 FireballEntity fireballentity = new FireballEntity(world, this.wizard, d2, d3, d4);
                 fireballentity.explosionPower = this.wizard.getExplosionPower();
                 fireballentity.setPos(this.wizard.getX() + vector3d.x * 4.0D, this.wizard.getY(0.5D) + 0.5D, fireballentity.getZ() + vector3d.z * 4.0D);
                 world.addFreshEntity(fireballentity);
+                
                 this.chargeTime = 0;
             }
         } else if (this.chargeTime > 0) {
@@ -81,37 +82,6 @@ public class FireballAttackGoal extends Goal{
         this.wizard.setCharging(this.chargeTime > 10);
     }
 
-    public void lightningTick() {
-        LivingEntity entity = this.wizard.getTarget();
-        double d0 = 64.0D;
-        if (entity.distanceToSqr(this.wizard) < 4096.0D && this.wizard.canSee(entity)) {
-            World world = this.wizard.level;
-            ++this.chargeTime;
-            if (this.chargeTime == 5 && !this.wizard.isSilent()) {
-
-            }
-            if (this.chargeTime == 10) {
-                double d1 = 4.0D;
-                Vector3d vector3d = this.wizard.getViewVector(1.0F);
-                double d2 = entity.getX() - (this.wizard.getX() + vector3d.x * 4.0D);
-                double d3 = entity.getY(0.5D) - (0.5D + this.wizard.getY(0.5D));
-                double d4 = entity.getZ() - (this.wizard.getZ() + vector3d.z * 4.0D);
-                if (!this.wizard.isSilent()) {
-                    world.levelEvent((PlayerEntity)null, 1016, this.wizard.blockPosition(), 0);
-                }
-
-                LightningBoltEntity lightningboltentity = EntityType.LIGHTNING_BOLT.create(world);
-                lightningboltentity.setDamage(wizard.getLightningDamage());
-                lightningboltentity.setPos(this.wizard.getX() + vector3d.x * 4.0D, this.wizard.getY(0.5D) + 0.5D, lightningboltentity.getZ() + vector3d.z * 4.0D);
-                world.addFreshEntity(lightningboltentity);
-                this.chargeTime = -20;
-            }
-        } else if (this.chargeTime > 0) {
-            --this.chargeTime;
-        }
-
-        this.wizard.setCharging(this.chargeTime > 10);
-    }
 	
 }
 
